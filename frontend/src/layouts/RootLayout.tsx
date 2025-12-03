dc5183012ca66090beed8b92c6faba1360ce3736
xinsheng04 <freddy161204@gmail.com>
2025-12-03 17:49:53 +0800

    Fixed the background image issue

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../component/navbar';
+import background from "../assets/background.jpg";
 
 const RootLayout: React.FC = () => {
   return (
@@ -8,6 +9,11 @@ const RootLayout: React.FC = () => {
       <Navbar />
       <main className="pt-16">
         <Outlet />
+        <img
+          className='fixed top-0 left-0 w-full h-full -z-10 bg-no-repeat bg-center bg-fixed bg-cover'
+          src={background}
+          alt="Background"
+        />
       </main>
     </>
   );
