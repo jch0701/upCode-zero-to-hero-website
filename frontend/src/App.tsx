d05792fc71d788a7a49e402523bd5b2728e71317
Liang <chanjl1@outlook.com>
2025-12-05 00:49:42 +0800

    new changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,10 @@ import { RoadmapDetails } from "./page/RoadmapDetails.tsx";
 import { Project } from "./page/Project.tsx";
 import { Career } from "./page/Career.tsx";
 import { Profile } from "./page/Profile.tsx";
-import { Login } from "./page/Login.tsx";
+import  Login_Pg  from "./page/Login_Pg.tsx";
+import Signup_Pg from "./page/Signup_Pg.tsx";
+import ForgotPassword_Pg from "./page/ForgotPassword_Pg.tsx";
+import ResetPassword_Pg from "./page/ResetPassword_Pg.tsx";
 import RootLayout from './layouts/RootLayout.tsx';
 import { RoadmapChapter } from "./page/RoadmapChapter.tsx";
 import { RoadmapDetailEdit } from "./page/RoadmapDetailEdit.tsx";
@@ -20,7 +23,7 @@ function App() {
       <Routes>
         <Route path="/" element={<RootLayout />}>
           <Route index element={<Overview />} />
-          <Route path="login" element={<Login />} />
+          <Route path="login" element={<Login_Pg />} />
           <Route path="roadmap" element={<Roadmap />} />
           <Route path="roadmap/:roadmapID/:roadmapSlug" element={<RoadmapDetails />} />
           <Route path="roadmap/:roadmapID/:roadmapSlug/:chapterID/:chapterSlug" element={<RoadmapChapter />} />
@@ -28,6 +31,9 @@ function App() {
           <Route path="project" element={<Project />} />
           <Route path="career" element={<Career />} />
           <Route path="profile" element={<Profile />} />
+          <Route path="signup" element={<Signup_Pg />} />
+          <Route path="forgot-password" element={<ForgotPassword_Pg />} />
+          <Route path="reset-password" element={<ResetPassword_Pg />} />
         </Route>
       </Routes>
     </Router>
