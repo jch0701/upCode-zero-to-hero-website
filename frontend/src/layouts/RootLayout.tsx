a6e4dacb8fe92eab89c56db4c9129bedb4a83fcf
zh2eng <zh2eng2022@gmail.com>
2025-12-10 23:55:36 +0800

    Always scroll to the top when pages changes

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -3,11 +3,23 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../component/navbar';
 import background from "../assets/background.jpg";
 import Footer from '@/component/footer';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const ScrollToTop: React.FC = () => {
+    // Get the current location object from react-router-dom
+    const { pathname } = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0); // Scrolls the window to the top (x=0, y=0)
+    }, [pathname]);
+    return null;
+};
 
 const RootLayout: React.FC = () => {
   return (
     <>
     <div className="relative flex flex-col min-h-screen">
+      <ScrollToTop />
       <img
         className='fixed top-0 left-0 w-full h-full -z-10 object-cover'
         src={background}
